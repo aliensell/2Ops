c0192a0137f3a29313550b2e96b161de649a526a
aliensell <59515165+aliensell@users.noreply.github.com>
2025-03-14 16:15:26 +0200

    Update Create_storage_for_terraform_state.sh

--- Create_storage_for_terraform_state.sh
+++ Create_storage_for_terraform_state.sh
@@ -4,11 +4,31 @@ RESOURCE_GROUP_NAME=terraform-backend-rg
 STORAGE_ACCOUNT_NAME=tfbackendstorage
 CONTAINER_NAME=tfstate
 
-# Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location westeurope
+# Check if resource group exists (if yes skip, if no create)
+az group show --name $RESOURCE_GROUP_NAME &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Resource group does not exist. Creating resource group..."
+    az group create --name $RESOURCE_GROUP_NAME --location eastus
+else
+    echo "Resource group $RESOURCE_GROUP_NAME already exists."
+fi
 
-# Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --location westeurope --sku Standard_LRS --encryption-services blob
+# Check if storage account already exists (if yes skip, if no create)
+STORAGE_ACCOUNT_EXIST=$(az storage account check-name --name $STORAGE_ACCOUNT_NAME --query 'nameAvailable' -o tsv)
 
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+if [ "$STORAGE_ACCOUNT_EXIST" == "true" ]; then
+    echo "Storage account name $STORAGE_ACCOUNT_NAME is available. Creating storage account..."
+    az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+else
+    echo "Storage account $STORAGE_ACCOUNT_NAME already exists. Skipping creation."
+fi
+
+# Check if container exists (if yes skip, if no create)
+CONTAINER_EXIST=$(az storage container list --account-name $STORAGE_ACCOUNT_NAME --query "[?name=='$CONTAINER_NAME'].name" -o tsv)
+
+if [ -z "$CONTAINER_EXIST" ]; then
+    echo "Container $CONTAINER_NAME does not exist. Creating container..."
+    az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+else
+    echo "Container $CONTAINER_NAME already exists. Skipping creation."
+fi
