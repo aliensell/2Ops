0394d7850cffce2b7ba0c70ee8c41798c4f522d3
aliensell <59515165+aliensell@users.noreply.github.com>
2025-03-15 18:35:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${var.environment}"
   resource_group_name  = azurerm_resource_group.dev_rg[0].name
   virtual_network_name = azurerm_virtual_network.dev_vnet[0].name
-  address_prefixes     = ["10.0.4.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
 }
 
@@ -71,7 +71,7 @@ resource "azurerm_subnet" "subnet_aks" {
   name                 = "subnet-aks-${var.environment}"
   resource_group_name  = azurerm_resource_group.dev_rg[0].name
   virtual_network_name = azurerm_virtual_network.dev_vnet[0].name
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -88,6 +88,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vnet_subnet_id     = azurerm_subnet.subnet_aks[0].id
   }
 
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+    service_cidr = "10.0.4.0/24"
+  }
+  
   private_cluster_enabled = true
   
   identity {
