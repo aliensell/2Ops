da73e77e726522be33941ccdc307e7ddf0434282
aliensell <59515165+aliensell@users.noreply.github.com>
2025-03-15 16:27:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,6 +74,7 @@ resource "azurerm_storage_account" "dev_storage" {
   account_replication_type = "LRS"
   network_rules {
     default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
     virtual_network_subnet_ids = [azurerm_subnet.subnet[0].id]
   }
 }
