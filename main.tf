378230a16e04b1362ff96cb96d8aad107d4c9a44
aliensell <59515165+aliensell@users.noreply.github.com>
2025-03-15 14:55:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "azurerm_key_vault" "qa_keyvault" {
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
-    key_permissions     = [ "Encrypt", "Decrypt", "Sign", "Verify", "List" ]
+    key_permissions     = [ "Get", "List", "Create", "Delete" ]
     secret_permissions  = [ "Get", "List" ]
     storage_permissions = [ "Get", "List" ]
   }
